3d874661ad0767fd7cff6f744566163f7de4d57c
David <davicolo717@gmail.com>
2025-06-18 20:47:44 -0400

    draft

--- draft.py
+++ draft.py
@@ -13,6 +13,11 @@ s_time = time.time()
 
 user_phr = input(f"Escribe: {phr}\n")
 
+if user_phr == phr:
+    print("¡Está bien escrito!")
+else:
+    print("¡Te has equivocado en algo!")
+
 e_time = time.time()
 
-print(f"Has tardado {e_time - s_time:.2f}s")
+print(f"¡Has tardado {e_time - s_time:.2f}s!")
